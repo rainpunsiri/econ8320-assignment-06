44a3e8a5fff4075f883a3809f4245e230104101c
rainpunsiri <rainpunsiri@gmail.com>
2024-10-09 16:01:20 -0500

    Update testDataFrame.py

--- tests/testDataFrame.py
+++ tests/testDataFrame.py
@@ -24,7 +24,7 @@ class testCases(unittest.TestCase):
       test = True
 
       for i in columns:
-        if i not in occupancy.columns:
+        if i not in Occupancy.columns:
           test = False
 
       self.assertTrue(test, "Your columns don't match the columns in the answer key.")
